9f7e0ded8fe24417fe374f48330a137450cb65f0
Daniela Brenes <danibreo6@gmail.com>
2022-03-05 03:12:52 -0600

    Initial app

--- App Móvil/TABAS/TABAS/MainActivity.cs
+++ App Móvil/TABAS/TABAS/MainActivity.cs
@@ -1,60 +1,74 @@
-﻿using System;
-using Android.App;
+﻿using Android.App;
 using Android.OS;
-using Android.Runtime;
-using Android.Views;
-using AndroidX.AppCompat.Widget;
 using AndroidX.AppCompat.App;
-using Google.Android.Material.FloatingActionButton;
-using Google.Android.Material.Snackbar;
+using System.Net;
+using Android.Content;
+using Android.Widget;
+using Android.Support;
+//using Newtonsoft.Json;
 
 namespace TABAS
 {
+    /// <summary>
+    /// This class represents the first view seen when the app is opened.
+    /// It inherits from the base class for Android activities
+    /// </summary>
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme.NoActionBar", MainLauncher = true)]
     public class MainActivity : AppCompatActivity
     {
+        private Button _signInButton;
+        private EditText _userId;
+        private EditText _userPassword;
+        private Toast _toast;
+        public const string Ipv4 = "192.168.1.2";
+
+        /// <summary>
+        /// This method is called when the activity is starting.
+        /// It contains the logic for the buttons shown in the first view.
+        /// </summary>
+        /// <param name="savedInstanceState"> a Bundle that contains the data the activity most recently
+        /// supplied if the activity is being re-initialized after previously being shut down. </param>
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
-            SetContentView(Resource.Layout.activity_main);
 
-            Toolbar toolbar = FindViewById<Toolbar>(Resource.Id.toolbar);
-            SetSupportActionBar(toolbar);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
 
-            FloatingActionButton fab = FindViewById<FloatingActionButton>(Resource.Id.fab);
-            fab.Click += FabOnClick;
-        }
+            _userId = FindViewById<EditText>(Resource.Id.signInId);
+            _userPassword = FindViewById<EditText>(Resource.Id.signInPassword);
+            _signInButton = FindViewById<Button>(Resource.Id.btnSignIn);
+            string toastText;
 
-        public override bool OnCreateOptionsMenu(IMenu menu)
-        {
-            MenuInflater.Inflate(Resource.Menu.menu_main, menu);
-            return true;
-        }
-
-        public override bool OnOptionsItemSelected(IMenuItem item)
-        {
-            int id = item.ItemId;
-            if (id == Resource.Id.action_settings)
+            // Manages the user info entered for the sign in
+            _signInButton.Click += (sender, args) =>
             {
-                return true;
-            }
+                var userEmailInput = _userId.Text;
+                var userPasswordInput = _userPassword.Text;
 
-            return base.OnOptionsItemSelected(item);
+                if (userEmailInput.Equals("") || userPasswordInput.Equals(""))
+                {
+                    toastText = "Debe ingresar la información solicitada.";
+                }
+
+                else
+                {
+                    /*using var webClient = new WebClient { BaseAddress = "http://" + MainActivity.Ipv4 + ":8080/CookTime_war/cookAPI/" };
+                    var url = "resources/auth?email=" + userEmailInput + "&password=" + userPasswordInput;
+                    webClient.Headers[HttpRequestHeader.ContentType] = "application/json";
+                    var send = webClient.DownloadString(url);
+
+                    var response = JsonConvert.DeserializeObject<string>(send);
+
+                    value = response;*/
+
+                    toastText = "Sesión iniciada";
+                }
+                _toast = Toast.MakeText(this, toastText, ToastLength.Short);
+                _toast.Show();
+            };
         }
-
-        private void FabOnClick(object sender, EventArgs eventArgs)
-        {
-            View view = (View) sender;
-            Snackbar.Make(view, "Replace with your own action", Snackbar.LengthLong)
-                .SetAction("Action", (View.IOnClickListener)null).Show();
-        }
-
-        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
-        {
-            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
-
-            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
-        }
-	}
+           
+    }
 }
