62bc5691a055258c8a9fe372c9297ec82c04c87c
BadStone3100 <48070861+Oscarmendez825@users.noreply.github.com>
2022-03-06 13:25:09 -0600

    Se agregó el poder crear maletas, además de las restricciones necesarias para la creación

--- RestAPI/TABAS/Models/Maleta.cs
+++ RestAPI/TABAS/Models/Maleta.cs
@@ -7,10 +7,10 @@ namespace TABAS.Models
 {
     public class Maleta
     {
-        public string cedulaUsuario { get; set; }
-        public string peso { get; set; }
-        public string costo { get; set; }
-        public string numero_maleta { get; set; }
+        public int cedulaUsuario { get; set; }
+        public int peso { get; set; }
+        public int costo { get; set; }
+        public int numero_maleta { get; set; }
 
     }
 }
