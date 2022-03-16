c6bc5916455a1b40a0fbe713c284936f1e405126
BadStone3100 <48070861+Oscarmendez825@users.noreply.github.com>
2022-03-16 02:09:09 -0600

    LogIn listo y modificación de la navbar por una mejor

--- FrontEnd/src/app/pages/login/login.component.ts
+++ FrontEnd/src/app/pages/login/login.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from 'src/app/services/auth.service';
+import { EstadoModel } from '../models/estado-model';
+import { TrabajadorModel } from '../models/trabajador-model.model';
 
 @Component({
   selector: 'app-login',
@@ -8,25 +10,28 @@ import { AuthService } from 'src/app/services/auth.service';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  trabajadorLogIn: TrabajadorModel =
+  {
+    Nombre: '',
+    Apellido: '',
+    Cedula: 0,
+    contrasena: '',
+    rol: ''
+  }
 
-  constructor(private Auth:AuthService,private router:Router) { }
+  constructor(private Auth:AuthService) { }
 
   ngOnInit(): void {}
 
   loginUser(event: { preventDefault: () => void; target: any; }){
 
-    event.preventDefault()
-    const target= event.target
-    const cedula= target.querySelector("#cedula").value
-    const password= target.querySelector("#password").value
+    event.preventDefault();
+    const target= event.target;
+    const cedula= target.querySelector("#cedula").value;
+    const password= target.querySelector("#password").value;
 
-    if (password=="aldo" && cedula=="123"){
-      window.alert("Hola mundo")
-      this.router.navigate(["home"])
-      this.Auth.setLoggedIn(true)
-    }else{
-      window.alert("XD")
-    }
-    console.log(cedula,password)
+    this.trabajadorLogIn.Cedula = cedula;
+    this.trabajadorLogIn.contrasena = password;
+    this.Auth.getUserDetails(this.trabajadorLogIn);
   }
 }
