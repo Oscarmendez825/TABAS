7d690ef0051d41bde1f00a62f2594289f9d1bcfa
BadStone3100 <48070861+Oscarmendez825@users.noreply.github.com>
2022-03-17 18:57:58 -0600

    Agregar todo lo necesario para el nuevo reporte

---
+++ FrontEnd/src/app/pages/models/reporte-model.ts
@@ -0,0 +1,9 @@
+export interface ReporteModel {
+    numVuelo: number,
+    placaAvion: number,
+    capacidad: number,
+    tMaletasV: number,
+    tMaletasBC: number,
+    tMaletasRe: number,
+    BCId: number
+}
